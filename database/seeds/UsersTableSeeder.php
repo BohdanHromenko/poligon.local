d4bc39d98f179bf945fa04d6042a4775e473477c
Bohdan <hromenkobohdan@icloud.com>
2019-09-02 07:14:20 +0200

    Made Seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name'      => 'Author unknown',
+                'email'     => 'author_unknown@g.g',
+                'password'  => bcrypt(Str::random(16)),
+            ],
+            [
+                'name'      => 'Author',
+                'email'     => 'author1@g.g',
+                'password'  => bcrypt('123123'),
+            ],
+        ];
+
+        DB::table('users')->insert($data);
     }
 }
