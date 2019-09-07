31e51b3b81478f8e404512f8b20b34a3bf94555d
Bohdan <hromenkobohdan@icloud.com>
2019-09-07 11:03:43 +0200

    Created BlogPostController Created BlogPosRepository

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-09-05 20:48:48.
+    * Generated on 2019-09-07 08:28:24.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- app/Http/Controllers/Blog/Admin/BaseController.php
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use App\Http\Controllers\Blog\BaseController as GuestBaseController;
 
+/**
+ * Class BaseController
+ * The base controller for all blog
+ * controllers in the admin panel
+ *
+ * Must be the parent of all blog controllers
+ * @package App\Http\Controllers\Blog\Admin
+ */
 abstract class BaseController extends GuestBaseController
 {
     /**
@@ -11,7 +19,7 @@ abstract class BaseController extends GuestBaseController
      */
     public function __construct()
     {
-
+        // Initializing General Moments for the Admin Panel
     }
 
 }
