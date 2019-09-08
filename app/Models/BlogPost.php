cb914c7359235c682d60d68152625d610515b653
Bohdan <hromenkobohdan@icloud.com>
2019-09-08 10:25:40 +0200

    add configutations

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class BlogPost extends Model
 {
     use SoftDeletes;
+
+    /**
+     * Article Category.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        // The article belongs to the category
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    /**
+     * Article Author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        // The article belongs to the User
+        return $this->belongsTo(User::class);
+    }
 }
